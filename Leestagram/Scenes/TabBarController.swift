93a19113a6a4ecc4982937c5331c9a98c9327489
이종진 <ljjlife@icloud.com>
2021-10-15 15:46:21 +0900

    git init, add file

---
+++ Leestagram/Scenes/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  Leestagram
+//
+//  Created by 이종진 on 2021/10/12.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let feedViewController = UINavigationController(rootViewController: FeedViewController())
+        feedViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        let profileViewController = UINavigationController(rootViewController: ProfileViewController())
+        feedViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
+        
+        viewControllers = [feedViewController, profileViewController]
+    }
+}
---
+++ Leestagram/UIButton+.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton+.swift
+//  Leestagram
+//
+//  Created by 이종진 on 2021/10/13.
+//
+
+import UIKit
+
+extension UIButton {
+    func setImage(systemName: String) {
+        contentHorizontalAlignment = .fill
+        contentVerticalAlignment = .fill
+        
+        imageView?.contentMode = .scaleAspectFit
+        imageEdgeInsets = .zero
+        
+        setImage(UIImage(systemName: systemName), for: .normal)
+    }
+}
